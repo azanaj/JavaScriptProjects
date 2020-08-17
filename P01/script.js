80d67f53a8df04eca2b890130d58d555b67868c9
azanaj <azana@github.com>
2020-08-17 21:01:12 +0500

    script.js updated

---
+++ P01/script.js
@@ -0,0 +1,27 @@
+/*
+-Document object model (dom) 
+node 
+document
+      doctype
+      html*/
+
+//-we can use var, let or const
+
+// All functions
+// Function to show error
+function showError(input, message) {
+    const formControl = input.parentElement
+}
+
+
+//this an event listener for the form on submit
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (username.value === '') {}
+})
